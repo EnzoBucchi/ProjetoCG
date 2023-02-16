dd149dc3c0dc6eda6b2fbcb7365ac22757df5182
Enzo Bucchi <enzo.silva@vericode.com.br>
2023-02-16 10:38:16 -0300

    Funções de conversão

--- main.py
+++ main.py
@@ -1,21 +1,73 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import cv2 as cv
+import math as mt
 from PIL import Image
 from streamlit_image_coordinates import streamlit_image_coordinates
 
-arquivo_imagem = st.file_uploader(
+### FUNÇÕES DE CONVERSÃO ###
+def rgb_to_cmy(rgb_array):
+    return [(255 - rgb_array[0]), (255 - rgb_array[1]), (255 - rgb_array[2])]
+
+def rgb_to_hsi(rgb_array):
+    r = rgb_array[0] / (rgb_array[0] + rgb_array[1] + rgb_array[2])
+    g = rgb_array[1] / (rgb_array[0] + rgb_array[1] + rgb_array[2])
+    b = rgb_array[2] / (rgb_array[0] + rgb_array[1] + rgb_array[2])
+    
+    if b <= g:
+        h = mt.acos(((0.5 * ((r - g) + (r - b))) / mt.pow((mt.pow((r - g), 2) + (r - b) * (g - b)), 0.5)))
+    else:
+        h = 2 * mt.pi - (mt.acos(((0.5 * ((r - g) + (r - b))) / mt.pow((mt.pow((r - g), 2) + (r - b) * (g - b)), 0.5))))
+
+    s = 1 - 3 * min(r, g, b)
+    i = (rgb_array[0] + rgb_array[1] + rgb_array[2]) / (3 * 255)
+    
+    H = round((h * 180) / mt.pi)    #PRECISA CONSERTAR
+    S = format((s * 100), ".2f")    #PRECISA CONSERTAR
+    I = round(i * 255)
+    
+    return [(H), (S), (I)] 
+
+### MONTAGEM DA INTERFACE ###
+arq_image = st.file_uploader(
     "Arquivo da Imagem:",
     help="Faça upload de uma imagem",
 )
 
-if arquivo_imagem is not None:
-    imagem = Image.open(arquivo_imagem)
-    coordenadas = streamlit_image_coordinates(imagem)
-    matriz_img = np.array(imagem)
-    if coordenadas is not None:
-        rgb_pixel = (matriz_img[coordenadas["y"]][coordenadas["x"]])
-        st.write(rgb_pixel)
+if arq_image is not None:
+    image = Image.open(arq_image)
+    coords = streamlit_image_coordinates(image)
+    image_matrix = np.array(image)
+    
+    line1 = st.columns((1, 1, 1))
+    line2 = st.columns((1, 1, 1))
+    line3 = st.columns((1, 1, 1))
+    
+    if coords is not None:
+        rgb_pixel = (image_matrix[coords["y"]][coords["x"]])
+        cmy_pixel = rgb_to_cmy(rgb_pixel)
+        hsi_pixel = rgb_to_hsi(rgb_pixel)
+        
+        with line1[0]:
+            st.write("R = " + str(rgb_pixel[0]))
+        with line2[0]:
+            st.write("G = " + str(rgb_pixel[1]))
+        with line3[0]:
+            st.write("B = " + str(rgb_pixel[2]))
+            
+        with line1[1]:
+            st.write("C = " + str(cmy_pixel[0]))
+        with line2[1]:
+            st.write("M = " + str(cmy_pixel[1]))
+        with line3[1]:
+            st.write("Y = " + str(cmy_pixel[2]))
+            
+        with line1[2]:
+            st.write("H = " + str(hsi_pixel[0]))
+        with line2[2]:
+            st.write("S = " + str(hsi_pixel[1]))
+        with line3[2]:
+            st.write("I = " + str(hsi_pixel[2]))
+        
 
     
